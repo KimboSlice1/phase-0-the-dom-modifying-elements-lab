4ebd1cc0d01764f8e38db30238639e04c83cb726
Kimber Herron <kh582013@gmail.com>
2022-09-10 21:56:05 -0400

    commit tree 64edbb74cbafea13967c61bfd0ee9ab3ad0295c5

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const theMain = document.getElementById("main");
+theMain.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Kimber is the champion";
+document.body.append(newHeader);
